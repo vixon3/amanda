36133b364081fee0197fae3d6fc2f6ea2fb7c6e4
vixon3 <163432217+vixon3@users.noreply.github.com>
2025-07-09 23:58:57 -0400

    First lool

---
+++ code1.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/hogar')
+def hogar():
+    return render_template("hogar.html")
+
+@app.route('/index', methods=['POST'])
+def recibir_ubicacion():
+    data = request.get_json()
+    print("📍 Ubicación recibida:", data)
+    return {'status': 'ok'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
